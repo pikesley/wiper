cefeb7c92733d27c93290b16916d83d85dd1e99c
Sam Pikesley <sam.pikesley@gmail.com>
2018-02-10 17:22:50 +0000

    First pattern works

--- lib/wiper/squeegee.rb
+++ lib/wiper/squeegee.rb
@@ -1,11 +1,26 @@
 module Wiper
   class Squeegee
-    attr_accessor :starter
+    attr_accessor :grid
 
     def initialize
-      @starter = [[0] * 45] * 7
-
+      @grid = [[0] * 45] * 7
       yield self if block_given?
+      @index = 0
+    end
+
+    def wipe
+      @grid.map do |row|
+        row[@index] = 1
+        row[@index - 1] = 0 #if @index > 0
+      end
+      @index += 1
+    end
+
+    def each
+      while @index < 45
+        wipe
+        yield @grid
+      end
     end
   end
 end
--- spec/wiper/squeegee_spec.rb
+++ spec/wiper/squeegee_spec.rb
@@ -4,7 +4,59 @@ module Wiper
       it 'has defaults' do
         s = Squeegee.new
 
-        expect(s.starter).to eq [[0] * 45] * 7
+        expect(s.grid).to eq [[0] * 45] * 7
+      end
+
+      it 'takes a block' do
+        s = Squeegee.new do |s|
+          s.grid = [[1] * 45] * 7
+        end
+
+        expect(s.grid[0][0]).to eq 1
+      end
+    end
+
+    context 'simple wipe' do
+      s = Squeegee.new
+
+      it 'draws the first column' do
+        s.wipe
+
+        s.grid.each do |row|
+          expect(row[0]).to eq 1
+          expect(row[1..44].all? { |i| i == 0 }).to be true
+        end
+      end
+
+      it 'cleans up after itself' do
+        s.wipe
+
+        s.grid.each do |row|
+          expect(row[0]).to eq 0
+          expect(row[1]).to eq 1
+          expect(row[2..44].all? { |i| i == 0 }).to be true
+        end
+      end
+
+      it 'goes all the way across' do
+        43.times do
+          s.wipe
+        end
+
+        s.grid.each do |row|
+          expect(row[0..43].all? { |i| i == 0 }).to be true
+          expect(row[44]).to eq 1
+        end
+      end
+
+      it 'exposes an iterator' do
+        sq = Squeegee.new
+
+        count = 0
+        sq.each do |grid|
+          expect(grid[0].index 1).to eq count
+          count += 1
+        end
       end
     end
   end
