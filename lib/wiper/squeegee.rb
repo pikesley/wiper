6f74f18e5fcad64abac2f51300b68389b5b2605a
Sam Pikesley <sam.pikesley@gmail.com>
2018-02-10 22:39:58 +0000

    Muuuuch tidier

--- lib/wiper/squeegee.rb
+++ lib/wiper/squeegee.rb
@@ -7,38 +7,20 @@ module Wiper
       @direction = :east
       yield self if block_given?
       @index = 0
-      @index = 44 if @direction == :west
     end
 
     def wipe
-      case @direction
-      when :east
-        @grid.map do |row|
-          row[@index] = 1 if @index < 45
-          row[@index - 1] = 0 if @index > 0
-        end
-        @index += 1
-
-      when :west
-        @grid.map do |row|
-          row[@index] = 1 if @index >= 0
-          row[@index + 1] = 0 if @index < 44
-        end
-        @index -= 1
+      @grid.map do |row|
+        row.reverse! if @direction == :west
+        row[@index] = 1 if @index < 45
+        row[@index - 1] = 0 if @index > 0
+        row.reverse! if @direction == :west
       end
-    end
-
-    def has_more?
-      case @direction
-      when :east
-        @index < 45
-      when :west
-        @index >= 0
-      end
+      @index += 1
     end
 
     def each
-      while has_more?
+      while @index < 45
         wipe
         yield @grid
       end
